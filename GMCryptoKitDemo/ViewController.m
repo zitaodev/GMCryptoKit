b6b1a349ef8b361bb74aba9ab04bcafb083a77ca
zitaodev <zitaodev@gmail.com>
2024-03-18 10:55:16 +0800

    示例Demo日志输出调整

--- GMCryptoKitDemo/ViewController.m
+++ GMCryptoKitDemo/ViewController.m
@@ -17,6 +17,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    NSMutableString *logStr = [NSMutableString stringWithString:@""];
+    // 生成指定字节的加密安全随机数
+    NSData *randomData = [GMRandomGenerator gm_secRandomDataWithLength:16];
+    [logStr appendString:@"\n-------安全随机数-------"];
+    [logStr appendFormat:@"\n随机数：%@", randomData];
+    
     // 生成SM2密钥对
     NSDictionary *keyPair = [GMSm2Cryptor gm_createSm2KeyPair];
     NSData *publicKey = keyPair[@"publicKey"];
@@ -28,28 +34,25 @@
     NSData *ciphertextData = [GMSm2Cryptor gm_sm2EncryptData:plaintextData withPublicKey:publicKey];
     NSData *decryptedData = [GMSm2Cryptor gm_sm2DecryptData:ciphertextData withPrivateKey:privateKey];
     
-    NSMutableString *encAndDecStr = [NSMutableString stringWithString:@""];
-    [encAndDecStr appendString:@"\n-------SM2加密与解密-------"];
-    [encAndDecStr appendFormat:@"\nSM2明文：%@", plaintextData];
-    [encAndDecStr appendFormat:@"\nSM2公钥：%@", publicKey];
-    [encAndDecStr appendFormat:@"\nSM2私钥：%@", privateKey];
-    [encAndDecStr appendFormat:@"\nSM2加密密文：%@", ciphertextData];
-    [encAndDecStr appendFormat:@"\nSM2解密结果：%@", decryptedData];
-    NSLog(@"%@", encAndDecStr);
+    [logStr appendString:@"\n-------SM2加密与解密-------"];
+    [logStr appendFormat:@"\nSM2明文：%@", plaintextData];
+    [logStr appendFormat:@"\nSM2公钥：%@", publicKey];
+    [logStr appendFormat:@"\nSM2私钥：%@", privateKey];
+    [logStr appendFormat:@"\nSM2加密密文：%@", ciphertextData];
+    [logStr appendFormat:@"\nSM2解密结果：%@", decryptedData];
     
     // 数字签名和验证
     NSData *messageData = [GMRandomGenerator gm_secRandomDataWithLength:36]; // 待签名的数据
     NSData *signatureData = [GMSm2Cryptor gm_sm2SignData:messageData withPrivateKey:privateKey];
     BOOL isSignatureValid = [GMSm2Cryptor gm_sm2VerifySignature:signatureData forData:messageData withPublicKey:publicKey];
 
-    NSMutableString *sigAndVerStr = [NSMutableString stringWithString:@""];
-    [sigAndVerStr appendString:@"\n-------SM2签名与验签-------"];
-    [sigAndVerStr appendFormat:@"\nSM2签名消息：%@", messageData];
-    [sigAndVerStr appendFormat:@"\nSM2公钥：%@", publicKey];
-    [sigAndVerStr appendFormat:@"\nSM2私钥：%@", privateKey];
-    [sigAndVerStr appendFormat:@"\nSM2数字签名：%@", signatureData];
-    [sigAndVerStr appendFormat:@"\nSM2验签结果：%@", isSignatureValid ? @"验签成功" : @"验签失败"];
-    NSLog(@"%@", sigAndVerStr);
+    [logStr appendString:@"\n-------SM2签名与验签-------"];
+    [logStr appendFormat:@"\nSM2签名消息：%@", messageData];
+    [logStr appendFormat:@"\nSM2公钥：%@", publicKey];
+    [logStr appendFormat:@"\nSM2私钥：%@", privateKey];
+    [logStr appendFormat:@"\nSM2数字签名：%@", signatureData];
+    [logStr appendFormat:@"\nSM2验签结果：%@", isSignatureValid ? @"验签成功" : @"验签失败"];
+    NSLog(@"%@", logStr);
 }
 
 @end
