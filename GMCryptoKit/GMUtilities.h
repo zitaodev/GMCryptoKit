968e9f466a6008d2f71d24e36d7f0d00941b3fe9
zitaodev <zitaodev@gmail.com>
2024-05-21 15:56:24 +0800

    更新工具类注释

--- GMCryptoKit/GMUtilities.h
+++ GMCryptoKit/GMUtilities.h
@@ -12,18 +12,53 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface GMUtilities : NSObject
 
+/**
+ 十六进制编码的字符串转换为对应的UTF-8编码二进制数据
+
+ @return UTF-8编码二进制数据
+ */
 + (NSData *_Nullable)hexStringToData:(NSString *)hexString;
 
+/**
+ UTF-8编码二进制数据转换为对应的十六进制编码的字符串
+
+ @return 十六进制编码的字符串
+ */
 + (NSString *_Nullable)dataToHexString:(NSData *)data;
 
+/**
+ UTF-8编码字符串转换为对应的十六进制数据的字符串
+
+ @return 十六进制数据的字符串
+ */
 + (NSString *_Nullable)stringToHexString:(NSString *)string;
 
+/**
+ 十六进制数据的字符串转换为对应的UTF-8编码字符串
+
+ @return UTF-8编码字符串
+ */
 + (NSString *_Nullable)hexStringToString:(NSString *)hexString;
 
+/**
+ UTF-8编码字符串转换为对应的UTF-8编码二进制数据
+
+ @return UTF-8编码二进制数据
+ */
 + (NSData *_Nullable)stringToData:(NSString *)string;
 
+/**
+ UTF-8编码二进制数据转换为对应的Base64编码的字符串
+
+ @return Base64编码的字符串
+ */
 + (NSString *_Nullable)dataToBase64String:(NSData *)data;
 
+/**
+ Base64编码字符串转换为对应的UTF-8编码二进制数据
+
+ @return UTF-8编码二进制数据
+ */
 + (NSData *_Nullable)base64StringToData:(NSString *)base64String;
 @end
 
